5c2adff402ae823ca240d511423ef7a55ce0d4ed
juancarlossrp-ms <jjcarlos853@gmail.com>
2022-06-06 12:00:07 -0300

    lets

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="whatsapp://send?abid=+5567999581001&text=Olá!"><img src="/faixa.jpg" className="App-logo" alt="logo" /></a>
+      </header>
+      <div className="div-dev">
+
+        <h1>
+          Site em desenvolvimento
+        </h1>
+      
+      </div>
+    </div>
+  );
+}
+
+export default App;
